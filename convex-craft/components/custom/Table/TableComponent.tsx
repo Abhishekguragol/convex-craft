677bc88cbfdfc4efd501f5ed1c171cd98c6acd4c
Rahul Suresh <rahs98@gmail.com>
2024-02-06 20:33:25 -0500

    Table component

---
+++ convex-craft/components/custom/Table/TableComponent.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+} from "@nextui-org/react";
+
+const TableComponent: React.FC<{
+    label: string;
+    headers: any[];
+    data: any[][];
+}> = ({ label, headers, data }) => {
+    return (
+        <div className="component">
+            <Table aria-label={label}>
+                <TableHeader>
+                    {headers.map((header) => (
+                        <TableColumn>{header}</TableColumn>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {data.map((row, ind) => {
+                        return (
+                            <TableRow key={ind}>
+                                {row.map((col) => {
+                                    return <TableCell>{col}</TableCell>;
+                                })}
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
+
+export default TableComponent;
